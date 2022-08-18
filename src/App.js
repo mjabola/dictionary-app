29b0961bb0bc9c79a1e96654876d870fb5498dc9
mjabola <99804271+mjabola@users.noreply.github.com>
2022-08-18 20:32:58 +1000

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './header.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+      <p className="text-center">Coming Soon</p>
+    </div>
+  );
+}
+
+export default App;
