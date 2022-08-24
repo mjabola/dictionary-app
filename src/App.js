9a9967687788ef52bc6199df0f61da26dcf9ad41
mjabola <99804271+mjabola@users.noreply.github.com>
2022-08-25 09:07:51 +1000

    Revert "footer added"
    This reverts commit 08ff5f58b627cbf800861d86787750cd5b9197f4.

--- src/App.js
+++ src/App.js
@@ -6,28 +6,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
-        <main>
-          <Dictionary />
-        </main>
-      </div>
-      <div className="bottom-container">
-        This project was coded by{" "}
-        <strong>
-          <a href="https://www.linkedin.com/in/margarita-jabola/ ">
-            Margarita Jabola
-          </a>
-        </strong>
-        <br />
-        This is open-sourced on{" "}
-        <a href="https://github.com/mjabola/dictionary-app">GitHub</a> and
-        hosted on{" "}
-        <a href="https://resplendent-dodol-db0f86.netlify.app/">Netlify</a>
-      </div>
+    <div className="App container">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+      <main>
+        <Dictionary />
+      </main>
     </div>
   );
 }
