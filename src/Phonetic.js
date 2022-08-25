5fe60f7fc2db45f1b947cb6c07bf0f855da549c5
mjabola <99804271+mjabola@users.noreply.github.com>
2022-08-25 19:36:40 +1000

    Bug fix

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Phonetic(props) {
-    return (
-        <div className="Phonetic">
-            {props.phonetic.text} <a href={props.phonetic.audio} target="_blank">Listen</a> <small>(Opens in a new window)</small>
-            </div>
-    );
+  return (
+    <div className="Phonetic">
+      {props.phonetic.text}{" "}
+      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
+        Listen
+      </a>{" "}
+      <small>(Opens in a new window)</small>
+    </div>
+  );
 }
